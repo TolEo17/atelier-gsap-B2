3454e8ad2bb212259d70623097412b87ec17e60c
Theo Moreau Gendre <theomoreaugendre@MacBook-Pro-de-Theo.local>
2025-02-21 09:51:44 +0100

    Adding HTML CSS AND JAVASCRIPT/GSAP

--- js/main.js
+++ js/main.js
@@ -1,19 +1,119 @@
-// GSAP Modules
+// GSAP Modules -------------------------------------------------------------
 gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
 
-// GSAP Demo Animation
 
-gsap.to(".profilsNetflix", {
-    scrollTrigger: {
-        trigger: "#homePageNetflix",
+// SECTION 1 ----------------------------------------
+
+let mm = gsap.matchMedia();
+mm.add({
+    isMobile: '(max-width: 799px)', 
+    isDesktop: '(min-width: 800px)',
+}, (context) => {
+    let {isMobile, isDesktop} = context.conditions; 
+}
+)
+
+let tlIntro = gsap.timeline({
+    scrollTrigger:{
+        trigger:".Section1",
         markers: true,
-        id: "#homePageNetflix",
-        start: "top 10%",
-        end: "bottome 90%",
-        toggleActions: "play none none none",
+        id:"Section1",
+        start:"top 0%",
+        end: "+=2000",
+        pin: true,
         scrub: 1,
+        toggleActions: "play none reverse reset",
+    }
+})
 
+tlIntro.to(".carreSection1",{
+    height: '40vh',
+    width: '100%',
+    duration: 2,
+})
+.to(".textCarreSection",{
+    opacity: 1,
+    duration: 8,
+})
+.to("#nameSec1",{
+    opacity: 0,
+    duration: 8,
+})
+
+
+
+
+
+// SECTION 2 -------------------------------------
+
+gsap.to(".logoEsd", {
+    duration: 5,
+    motionPath:{
+        path:'M1.49998 487C0.333314 431.667 27.9 316.6 147.5 299C267.1 281.4 315.333 212.667 324.5 180.5C343 130 408.7 23.4 523.5 1',
+        autoRotate: true,
     },
-    rotation: 360,
-    duration: 2
-,})
+    y: '100px',
+    scrollTrigger:{
+        trigger:".Section2",
+        markers: true,
+        id:"Section2",
+        start:"top 0%",
+        end: "+=1000",
+        pin: true,
+        scrub: 1,
+        toggleActions: "play none reverse reset",
+    }
+})
+
+
+// SECTION 3 ----------------------------------------------------------
+
+
+let timelineLogiciel1 = gsap.timeline({
+    scrollTrigger:{
+        trigger:"#section3",
+        markers: true,
+        id:"section3",
+        start:"top 0%",
+        end: "+=2000",
+        pin: true,
+        scrub: 1,
+        toggleActions: "play none reverse reset",
+    }
+});
+
+timelineLogiciel1.to(".illustrator",{x:100,})
+.to(".illustrator",{y:200,})
+.to(".illustrator",{x:-100,})
+.to(".illustrator",{y:-10,})
+//Photoshop
+.to(".photoshop",{x:100,})
+.to(".photoshop",{y:200,})
+.to(".photoshop",{x:-100,})
+.to(".photoshop",{y:-10,})
+//HTML
+.to(".html",{x:100,})
+.to(".html",{y:200,})
+.to(".html",{x:-100,})
+.to(".html",{y:-10,})
+//CSS
+.to(".css",{x:100,})
+.to(".css",{y:200,})
+.to(".css",{x:-100,})
+.to(".css",{y:-10,})
+/*
+//Illustrator RESET
+.to(".illustrator",{x:100,})
+//Photoshop RESET
+.to(".photoshop",{x:100,})
+//HTML RESET
+.to(".html",{x:100,})
+//CSS RESET
+.to(".css",{x:100,})
+*/
+
+// SECTION 4 ----------------------------------------------------------
+
+Draggable.create(".photoDeProfil", {
+    type: "x,y",
+})
